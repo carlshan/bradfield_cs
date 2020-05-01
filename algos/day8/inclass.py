f148d484b25fa74ec1c6db926aa7ed607aef5c59
Carl Shan <shan.carl@gmail.com>
2020-04-30 19:00:25 -0700

    Updating backtracking so I can recover the steps to the puzzle

--- algos/day8/inclass.py
+++ algos/day8/inclass.py
@@ -69,6 +69,37 @@ def bfs_search(graph, start, end):
 
 # print(bfs_search(graph, 1, 4))
 
+def recover(end, graph):
+    # given a graph and ending state, find the sequence of events that led to it from (0, 0)
+    # used in the following water jug puzzle
+    # another thought: each answer in the solution should only have 1 unique parent state
+    curr = end
+    path = [end]
+
+    def helper(curr, graph):
+        for state in graph:
+            for child in graph[state]:
+                if child == curr:
+                    return state
+
+    while curr != (0, 0):
+        # Loop through the graph and find the first occurring state equal to curr
+        # end once we're at the beginning
+        curr = helper(curr, graph)
+        path.append(curr)
+
+    return path[::-1] # reverse to see how to start at the beginning
+
+def recover2(start, end, came_from):
+    # Using the `came_from` return value to recover the path
+    path = []
+    curr = end
+    while curr != start:
+        path.append(curr)
+        curr = came_from[curr]
+    path.append(start)
+    return path[::-1]
+
 def solve_puzzle(A, B, target):
     jug1 = 0
     jug2 = 0
@@ -76,16 +107,19 @@ def solve_puzzle(A, B, target):
     q = collections.deque()
     values = (jug1, jug2)
     q.append((values, 0))
+    came_from = dict()
     visited = set()
 
     graph = dict()
-    graph[values] = [] # other tuples that will contain possible states of (jug1, jug2) to go into
+    graph[values] = [] # other tuples that will contain possible states of (jug1, jug2)
+
     while len(q) > 0:
         (jug1, jug2), steps = q.popleft()
         values = (jug1, jug2)
         if jug1 == target or jug2 == target:
-            graph[values] = [] # adding this final step so I can see the final states of jug1 and jug2
-            return steps, graph
+            graph[values] = [] # so I can see the final states of jug1 and jug2
+            # correct_steps = recover(values, graph)
+            return steps, graph, came_from
 
         if values in visited:
             continue
@@ -108,15 +142,53 @@ def solve_puzzle(A, B, target):
         option6 = (jug1, 0)
 
         graph[values] = [option1, option2, option3, option4, option5, option6]
-
         visited.add(values)
         for child in graph[values]:
             q.append((child, steps + 1))
+            if child not in came_from:
+                came_from[child] = values
 
     return None
 
-answer, graph = solve_puzzle(3, 5, 4)
+answer, graph, came_from = solve_puzzle(3, 5, 4)
 
 import pprint
 
-pprint.pprint(graph)
+# pprint.pprint(graph)
+# pprint.pprint(came_from)
+start = (0, 0)
+end = (3, 4)
+print(recover2(start, end, came_from))
+
+
+# Definition for a binary tree node.
+# From: https://leetcode.com/problems/minimum-depth-of-binary-tree/submissions/
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+from collections import deque
+import math
+
+class Solution:
+    def minDepth(self, root):
+        # Recursive solution
+        def helper(tree):
+            """
+            Returns the minimum depth of tree
+            """
+            if tree is None:
+                return 0
+            elif tree.left is None and tree.right is None:
+                return 1
+            else:
+                left_val = math.inf
+                right_val = math.inf
+                if tree.left:
+                    left_val = 1 + helper(tree.left)
+                if tree.right:
+                    right_val = 1 + helper(tree.right)
+                return min(left_val, right_val)
+
+        return helper(root)
